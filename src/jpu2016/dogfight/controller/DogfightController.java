d957a350bea6cb09f898f76fc423290afe530b17
Fam Steve <Fam Steve@DESKTOP-3PSL3L8.mshome.net>
2021-04-30 15:46:43 +0100

    phase2

--- src/jpu2016/dogfight/controller/DogfightController.java
+++ src/jpu2016/dogfight/controller/DogfightController.java
@@ -2,4 +2,21 @@ package jpu2016.dogfight.controller;
 
 public class DogfightController implements IOrderPerformer {
 
+	private int TIME_SLEEP=30;
+	private IDogfightModel dogfightModel;
+	
+	public DogfightController(IDogfightModel dogfightModel) {
+	
+		this.dogfightModel= dogfightModel;
+		
+	public void orderPerform(UserOrder userOrder) {
+		this.userOrder= userOrder;
+	}
+	
+	public void Play() {
+		
+	}
+	
+	}
+	
 }
--- src/jpu2016/dogfight/controller/UserOrder.java
+++ src/jpu2016/dogfight/controller/UserOrder.java
@@ -1,5 +1,22 @@
 package jpu2016.dogfight.controller;
 
-public class UserOrder {
+public class UserOrder implements IUserOrder {
+
+	private int player;
+	private Order order;
+	
+	public UserOrder(int player, Order order) {
+		this.player= player;
+		this.order= order;
+	}
+
+	public int getPlayer() {
+		return player;
+	}
+
+	public Order getOrder() {
+		return order;
+	}
+	
 
 }
